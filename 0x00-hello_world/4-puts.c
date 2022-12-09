751213d9b60c246b06d0e7c5e06d2e421e6ae8bb
ruyadanep <ruya.danepp@gmail.com>
2022-12-09 02:03:09 -0800

    Task 5, C program that prints exactly with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- * main-entry
+/*
+ *main-entry
  *Return: Always 0
 */
-#include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	retun 0;
 
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ *Main-Entry
+ *Return: Always 0
+*/
+int main(void){
+	pintf("with proper grammar, but the outcome is a piece of art\n");
+	return 0;
+}
