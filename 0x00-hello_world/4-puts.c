e779996652809e371a7a7c1ae956b60728043d59
ruyadanep <ruya.danepp@gmail.com>
2022-12-09 02:34:49 -0800

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
- *main-entry
- *Return: Always 0
-*/
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return:Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	retun 0;
-
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
+
+
