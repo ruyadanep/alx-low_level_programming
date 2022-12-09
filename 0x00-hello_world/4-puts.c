a416105baf6651ee8bca24c98e4ad30887df5b98
ruyadanep <ruya.danepp@gmail.com>
2022-12-09 00:06:28 -0800

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 #include <stdio.h>
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle\n);
 	retun 0;
 
 }
