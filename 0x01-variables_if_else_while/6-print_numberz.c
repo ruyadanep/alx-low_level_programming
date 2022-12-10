ef559983dac00746d651036bc025ccf723a082ed
ruyadanep <ruya.danepp@gmail.com>
2022-12-09 17:57:58 -0800

    Task 6, program that prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - return single integer from 0-10
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
 
 	return (0);
 }
